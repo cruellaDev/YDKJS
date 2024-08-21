30c92ad2ed0c5145f83a27085f7140fa3149223f
Jiwon Lee <82017189+cruellaDev@users.noreply.github.com>
2024-08-21 17:29:00 +0900

    Create interpolation.js

---
+++ interpolation.js
@@ -0,0 +1,12 @@
+// 보간법 (interpolation), 문자 열 내 변수 표현식 (variable expression)
+
+let firstName = "카일";
+
+console.log("제 이름은 ${ firstName }입니다.");
+// 제 이름은 ${ firstName }입니다.
+
+console.log('제 이름은 ${ firstName }입니다.');
+// 제 이름은 ${ firstName }입니다.
+
+console.log(`제 이름은 ${ firstName }입니다.`);
+// 제 이름은 카일입니다.
